1021b7737f480d2a42bbb1952f76c44bcc46d0bc
luiz <luizsilvatraders@hotmail.com>
2022-12-10 14:51:45 -0300

    Exercicios concluído

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const prompt = require("prompt-sync")();
 
 // Leia os três números
-
+var n1 = parseInt(prompt("Digite o número 1: "));
+var n2 = parseInt(prompt("Digite o número 2: "));
+var n3 = parseInt(prompt("Digite o número 3: "));
 // Calcule a soma e a média
-
+var soma = n1 + n2 + n3;
+var media = soma / 3;
 // Mostre na tela o valor da soma e média
+console.log("\nA soma é:", soma);
+console.log("A média é:", media);
+//obs:estamos trabalhando com números inteiros(parseInt)
